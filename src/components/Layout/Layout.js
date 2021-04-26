e3c73cff6c2f70d91fb28fd1acf91ca690fe2b52
cr7yash <yashaqua@gmail.com>
2021-04-26 14:33:06 +0530

    Responsive adjustment and side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../UI/SideDrawer/SideDrawer";
 import "./Layout.css";
 
 const Layout = (props) => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <main className="Content">{props.children}</main>
   </Aux>
 );
