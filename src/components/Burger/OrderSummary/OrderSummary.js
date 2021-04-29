49fcb9518a8766ce67348f9fd2a0f526f76ad860
cr7yash <yashaqua@gmail.com>
2021-04-29 11:46:14 +0530

    Improving performance by checking for unnecessary renders

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,30 +4,39 @@ import Button from "../../UI/Button/Button";
 
 import "./OrderSummary.css";
 
-export default function OrderSummary(props) {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
-    return (
-      <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
-        {props.ingredients[igKey]}
-      </li>
+class OrderSummary extends React.Component {
+  componentDidUpdate() {
+    console.log("[OrderSummary] is updating");
+  }
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKey) => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+            {this.props.ingredients[igKey]}
+          </li>
+        );
+      }
     );
-  });
-  return (
-    <Aux>
-      <h3>Your Order </h3>
-      <p>You chose following ingredients</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCanceled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinue}>
-        CONTINUE
-      </Button>
-    </Aux>
-  );
+    return (
+      <Aux>
+        <h3>Your Order </h3>
+        <p>You chose following ingredients</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCanceled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinue}>
+          CONTINUE
+        </Button>
+      </Aux>
+    );
+  }
 }
+
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,19 +4,30 @@ import Backdrop from "../Backdrop/Backdrop";
 
 import "./Modal.css";
 
-export default function Modal(props) {
-  return (
-    <Aux>
-      <Backdrop clicked={props.modalClosed} show={props.show} />
-      <div
-        style={{
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: props.show ? "1" : "0"
-        }}
-        className="Modal"
-      >
-        {props.children}
-      </div>
-    </Aux>
-  );
+class Modal extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.state.show;
+  }
+
+  componentDidUpdate() {
+    console.log("[Modal] is updating");
+  }
+  render() {
+    return (
+      <Aux>
+        <Backdrop clicked={this.props.modalClosed} show={this.props.show} />
+        <div
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+          className="Modal"
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
 }
+
+export default Modal;
